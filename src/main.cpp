81a3f241f453398db5c90d0ee53c23f5064dfc3c
Miles Kent <mileskent13@gmail.com>
2024-02-20 01:20:55 -0800

    works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
@@ -9,24 +10,43 @@
 #define WINW WINDOWSIZE
 #define WINH WINDOWSIZE
 
-// Make wrapper struct for planet -> sf::CircleShape, mass
+constexpr int FPS = 60;
+constexpr float deltatime = 1.0f / FPS;
+
+#define G 10.0f
+
+class Planet : public sf::CircleShape{
+public:
+    float mass;
+    sf::Vector2<float> velocity;
+
+    Planet () : sf::CircleShape (WINDOWSIZE / 2.0f) {
+        this->mass = 0.0f;
+        this->velocity = sf::Vector2(0.0f, 0.0f);
+    }
+
+    Planet (float mass, float radius) : sf::CircleShape (radius) {
+        this-> mass = mass;
+        this->velocity = sf::Vector2(0.0f, 0.0f);
+    }
+
+};
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WINW, WINH), "Planets");
-    window.setFramerateLimit(60);
-
-    sf::CircleShape sun(WINDOWSIZE / 30.0f);
+    Planet sun(100, WINDOWSIZE / 30.0f);
     sun.setFillColor(sf::Color::Yellow);
     sun.setPosition(WINW / 2.0f, WINH / 2.0f);
 
-    sf::CircleShape earth(WINDOWSIZE / 90.0f);
+    Planet earth(1, WINDOWSIZE / 90.0f);
     earth.setFillColor(sf::Color::Blue);
     earth.setPosition(WINW / 1.5f, WINH / 2.0f);
 
-    std::vector<sf::CircleShape*> bodies = {&sun, &earth};
-
+    std::vector<Planet*> bodies = {&sun, &earth};
+    
+    sf::RenderWindow window(sf::VideoMode(WINW, WINH), "Planets");
+    window.setFramerateLimit(FPS);
     while (window.isOpen())
     {
         // EVENTS
@@ -39,9 +59,29 @@ int main()
 
         // GRAVITY
         // F_g = K * m_1 * m_2 * r^-2
-        for (auto body : bodies) {
-            // body->move ( sf::Vector2(0.0f, 1.0f) );
+        // Each Body Exerts a force on each other body
+        // For each body, determine the net force applied by the other bodies
+        for (int b = 0; b < bodies.size(); b++) {
+            sf::Vector2<float> netforce (0.0f, 0.0f);
+            for (int j = 0; j < bodies.size(); j++) {
+                if (j != b) {
+                    // direction
+                    sf::Vector2<float> force = bodies.at(j)->getPosition() - bodies.at(b)->getPosition();
+                    // unit direction
+                    float distance = sqrt((pow(force.x, 2) + pow(force.y, 2)));
+                    force /= distance;
+                    // final
+                    force *= G * bodies.at(j)->mass * bodies.at(b)->mass * (float)pow(distance, -2);
+                    netforce += force;
+                }
+            }
+            // Calculate velocity based on netforce vector
+            // netForce = m*a
+            //  a = netForce/m -> v = netForce/m * t
+            bodies.at(b)->velocity += netforce / bodies.at(b)->mass * deltatime;
+            bodies.at(b)->move(bodies.at(b)->velocity);
         }
+
 
         // NEW FRAME
 
